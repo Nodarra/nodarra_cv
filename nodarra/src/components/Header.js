bf5a31f38fbaed5d57224c7727d066d6a2a55ff9
Nodarra <sisicdino4@gamil.com>
2024-03-01 01:00:07 +0100

    Header Section added

---
+++ nodarra/src/components/Header.js
@@ -0,0 +1,14 @@
+import "../styles/header.css";
+
+export const Header = () => {
+  return (
+    <div className="headerContainer">
+        <div className="headerText">
+            <span>print(</span>"Hello, my name is Dino"<span>)</span>
+        </div>
+        <div className="headerImg">
+           
+        </div>
+    </div>
+  )
+}
